881089230c851b8aff2039e108df5f948233b662
Angel Osiel <ang_vzrz@live.com.mx>
2023-03-29 08:51:32 +0000

    Users List

---
+++ src/components/user/user-list/caption.tsx
@@ -0,0 +1,12 @@
+export function Caption() {
+  return (
+    <caption className="w-full">
+      <div className="mb-4 flex items-center justify-between">
+        <h2 className="text-2xl font-bold">Users List</h2>
+        <button className="relative rounded bg-amber-500 p-2 text-white">
+          ADD NEW USER
+        </button>
+      </div>
+    </caption>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,25 @@
 import Head from 'next/head';
 import styles from '@/styles/Home.module.css';
 
-export default function Home() {
+import { getUsers } from '@/services/users-api';
+import { User } from '@/types';
+import { useEffect, useState } from 'react';
+import { UserList } from '@/components/user';
+import { Header } from '@/components/ui';
+
+interface HomeProps {
+  users: User[];
+}
+
+export default function Home({ users }: HomeProps) {
+  const [usersLoaded, setUsersLoaded] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (users) {
+      setUsersLoaded(true);
+    }
+  }, [users]);
+
   return (
     <>
       <Head>
@@ -10,7 +28,15 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <Header />
+        {usersLoaded && <UserList users={users} />}
+      </main>
     </>
   );
 }
+
+export const getServerSideProps = async () => {
+  const usersData: User[] = await getUsers();
+  return { props: { users: usersData } };
+};
