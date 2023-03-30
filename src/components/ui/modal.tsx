adfdc3690f712fa0bcd383d0fe30f4b86d9d0452
angvzrz <ang_vzrz@live.com.mx>
2023-03-30 14:38:16 -0600

    Handle children component being loaded before rendering its contents in order to prevent white rectangle flicker

--- src/components/ui/modal.tsx
+++ src/components/ui/modal.tsx
@@ -1,16 +1,26 @@
+import { useEffect, useState } from 'react';
+
 interface ModalProps {
   children: React.ReactNode;
 }
 
 export function Modal({ children }: ModalProps) {
+  const [isContentLoaded, setIsContentLoaded] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsContentLoaded(true);
+  }, [children]);
+
   return (
     <dialog
       className="fixed inset-0 z-0  block  h-auto w-full border-none bg-black bg-opacity-80 "
       aria-modal="true"
     >
-      <div className="z-99 mx-auto w-full max-w-lg translate-y-1/2 rounded-xl bg-white p-5 shadow-lg">
-        {children}
-      </div>
+      {isContentLoaded && (
+        <div className="z-99 mx-auto w-full max-w-lg translate-y-1/2 rounded-xl bg-white p-5 shadow-lg">
+          {children}
+        </div>
+      )}
     </dialog>
   );
 }
