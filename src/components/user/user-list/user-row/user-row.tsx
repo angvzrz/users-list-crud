1567c13233adb815dd39e4c98905cf5d4f0f1f79
angvzrz <ang_vzrz@live.com.mx>
2023-03-31 01:31:24 -0600

    Handle setting the selected user. User row now receives an object of type User

--- src/components/user/user-list/user-row/user-row.tsx
+++ src/components/user/user-list/user-row/user-row.tsx
@@ -1,25 +1,27 @@
 import { DeleteButton, EditButton } from '@/components/ui';
-import { CrudActionContext } from '@/pages';
+import { AppContext } from '@/pages';
+import { User } from '@/types';
 import { useContext } from 'react';
 
 interface UserRowProps {
-  fullName: string;
-  email: string;
-  birthDate: string;
+  user: User;
 }
 
-export function UserRow({ fullName, email, birthDate }: UserRowProps) {
-  const { setCrudAction } = useContext(CrudActionContext);
+export function UserRow({ user }: UserRowProps) {
+  const { setCrudAction, setSelectedUser } = useContext(AppContext);
+  const { firstname, lastname, email, birth_date } = user;
+  const fullName = firstname + ' ' + lastname;
 
   const handleDeleteClick = () => {
     setCrudAction('user-delete');
+    setSelectedUser(user);
   };
 
   return (
     <tr className="hover:bg-gray-50">
       <td className="px-4 py-6">{fullName}</td>
       <td className="px-4 py-6">{email}</td>
-      <td className="px-4 py-6">{birthDate}</td>
+      <td className="px-4 py-6">{birth_date}</td>
       <td className="px-4 py-6">
         <div className="flex justify-end gap-4">
           <DeleteButton onClick={handleDeleteClick} />
