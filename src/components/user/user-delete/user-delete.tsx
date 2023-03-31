3514bd04134bcd3f824ef07d024e9583a8f95da1
angvzrz <ang_vzrz@live.com.mx>
2023-03-31 01:32:55 -0600

    Call delete user endpoint when confirming deletion. Refetch users data on sucess

--- src/components/user/user-delete/user-delete.tsx
+++ src/components/user/user-delete/user-delete.tsx
@@ -1,13 +1,20 @@
 import { DeleteIcon } from '@/components/ui';
-import { CrudActionContext } from '@/pages';
+import { AppContext } from '@/pages';
+import { deleteUser, getUsers } from '@/services/users-api';
 import { useContext } from 'react';
 
 export function UserDelete() {
-  const { setCrudAction } = useContext(CrudActionContext);
-  
+  const { setUsers, setCrudAction, selectedUser } = useContext(AppContext);
+
   const handleCancelClick = () => {
     setCrudAction('');
-  }
+  };
+
+  const handleDeleteClick = () => {
+    deleteUser(selectedUser.id)
+      .then(() => getUsers().then((data) => setUsers(data)))
+      .finally(() => setCrudAction(''));
+  };
 
   return (
     <div className="flex-auto justify-center p-5 text-center">
@@ -17,10 +24,16 @@ export function UserDelete() {
         Do you really want to delete this user? This process cannot be undone
       </p>
       <div className="mt-2 space-x-4 p-3 text-center md:block">
-        <button onClick={handleCancelClick} className="mb-2 rounded-full border bg-white px-5 py-2 text-sm font-medium tracking-wider text-gray-600 shadow-sm hover:bg-gray-100 hover:shadow-lg md:mb-0">
+        <button
+          onClick={handleCancelClick}
+          className="mb-2 rounded-full border bg-white px-5 py-2 text-sm font-medium tracking-wider text-gray-600 shadow-sm hover:bg-gray-100 hover:shadow-lg md:mb-0"
+        >
           Cancel
         </button>
-        <button className="mb-2 rounded-full border border-red-500 bg-red-500 px-5 py-2 text-sm font-medium tracking-wider text-white shadow-sm hover:bg-red-600 hover:shadow-lg md:mb-0">
+        <button
+          onClick={handleDeleteClick}
+          className="mb-2 rounded-full border border-red-500 bg-red-500 px-5 py-2 text-sm font-medium tracking-wider text-white shadow-sm hover:bg-red-600 hover:shadow-lg md:mb-0"
+        >
           Delete
         </button>
       </div>
